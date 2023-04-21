4faad590613085606721940fc16152e0a099d36a
Raju <Raju@LAPTOP-TF7ID873.bbrouter>
2023-04-21 17:36:24 +0530

    second commit

---
+++ src/april21/NewTest.java
@@ -0,0 +1,67 @@
+package april21;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+	@BeforeTest
+	  public void beforeTest() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+  @Test(dataProvider = "dp")
+  public void verify_login(String user, String pass) {
+	  driver.get("http://orangehrm.qedgetech.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  driver.findElement(By.name("txtUsername")).sendKeys(user);
+	  driver.findElement(By.name("txtPassword")).sendKeys(pass);
+	  driver.findElement(By.name("Submit")).click();
+	  String expected = "dashboard";
+	  String actual = driver.getCurrentUrl();
+	  if (actual.contains(expected))
+	  {
+		  Reporter.log("login success::"+expected+"   "+actual,true);
+	  }
+	  else
+	  {
+		  Reporter.log("login fail::"+expected+"    "+actual,true);
+	
+	  }
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+	  Object login [][]= new Object[4][2];
+	  login[0][0] = "Admin";
+	  login[0][1] = "Qedge123!@#";
+	  login[1][0] = "Admin1";
+	  login[1][1] = "Qedge123!@#";
+	  login[2][0] = "Admin";
+	  login[2][1] = "Qedge123!@#";
+	  login[3][0] = "Admin1";
+	  login[3][1] = "Qedge123!@#";
+	return login;
+	  
+   
+   
+  }
+  
+
+  @AfterTest
+  public void teardown() {
+	  driver.quit();
+  }
+
+}
+
